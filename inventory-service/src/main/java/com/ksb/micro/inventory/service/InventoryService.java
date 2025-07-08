31b5a203bf3ef817af47c9ebcfdcfb4cceb346d9
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-07-08 11:52:20 +0530

    logs, observation config file --added

--- inventory-service/src/main/java/com/ksb/micro/inventory/service/InventoryService.java
+++ inventory-service/src/main/java/com/ksb/micro/inventory/service/InventoryService.java
@@ -2,20 +2,21 @@ package com.ksb.micro.inventory.service;
 
 import com.ksb.micro.inventory.repository.InventoryRepository;
 /*import lombok.RequiredArgsConstructor;*/
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-/*@RequiredArgsConstructor*/
+@RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
-    //constructor manually added!!!
-    public InventoryService(InventoryRepository inventoryRepository) {
-        this.inventoryRepository = inventoryRepository;
-    }
-
     public boolean isInStock(String skuCode, Integer quantity){
+        log.info(" Start -- Received request to check stock for skuCode {}, with quantity {}", skuCode, quantity);
+        boolean isInStock = inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+        log.info(" End -- Product with skuCode {}, and quantity {}, is in stock - {}", skuCode, quantity, isInStock);
         //data JPA will create the query
-       return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+       return isInStock;
     }
 }
